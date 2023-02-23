7a936a27f11846d9d1ad6e4b37e8b23fa01ccbbe
mooscalia project <93492480+mooscaliaproject@users.noreply.github.com>
2023-02-23 13:26:42 -0300

    Feature target solutions on grasp

--- jobshop/heuristic/grasp/simple.py
+++ jobshop/heuristic/grasp/simple.py
@@ -7,8 +7,8 @@ from jobshop.heuristic.local_search import get_critical, local_search
 
 
 def grasp(
-    params: JobShopParams, maxiter=1000, alpha=(0.0, 1.0),
-    verbose=False, seed=None,
+    params: JobShopParams, maxiter=1000, alpha=(0.0, 1.0), target=None,
+    mixed_construction=True, verbose=False, seed=None,
 ):
     """Initialize a Pool a solutions using basic GRASP with minimal diversity
 
@@ -23,6 +23,13 @@ def grasp(
     alpha : float | tuple, optional
         Greediness parameter defined in the range (0, 1) in which 0 is random and 1 is greedy.
         If a tuple is passed a random uniform generator is used. By default (0.0, 1.0)
+    
+    target : float | int | None, optional
+        Taget that stops optimization, by default None
+    
+    mixed_construction : bool, optional
+        Either or not greedy makespan and time-remaining should be alternated
+        in the GRASP construction phase, by default True
     
     verbose : bool, optional
         Either or not to print messages while the algorithm runs, by default False
@@ -41,6 +48,10 @@ def grasp(
     else:
         get_alpha = lambda: alpha
     
+    # Initialize target if None
+    if target is None:
+        target = - float("inf")
+    
     # Initialize seed and solutions pool
     np.random.seed(seed)
     S_best = None
@@ -50,7 +61,7 @@ def grasp(
         
         # Initialize a solution S
         S = Graph(params.machines, params.jobs, params.p_times, params.seq)
-        if True:  # i % 2 == 0:
+        if (i % 2 == 0) or (not mixed_construction):
             semi_greedy_makespan(S, alpha=get_alpha())
         else:
             semi_greedy_time_remaining(S, alpha=get_alpha())
@@ -65,5 +76,9 @@ def grasp(
             C_best = S.C
             if verbose:
                 print(f"New best solution {C_best}")
+        
+        # Break if target
+        if C_best <= target:
+            break
     
     return S_best
