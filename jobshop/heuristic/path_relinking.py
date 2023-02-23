4e3b9afbb79e9079d17d65e21cfa5039284ed7eb
mooscalia project <93492480+mooscaliaproject@users.noreply.github.com>
2023-02-23 01:31:50 -0300

    Chore path relinking with memory

--- jobshop/heuristic/path_relinking.py
+++ jobshop/heuristic/path_relinking.py
@@ -1,5 +1,30 @@
+import numpy as np
 from jobshop.heuristic.operations import Graph
 from jobshop.heuristic.evaluation import calc_makespan
+
+
+class PathRelinking:
+    
+    def __init__(self) -> None:
+        self.visited_paths = {}
+    
+    @staticmethod
+    def get_delta_solutions(S: Graph, T: Graph):
+        return get_delta_solutions(S, T)
+    
+    @staticmethod
+    def get_delta_module(S: Graph, T: Graph):
+        return get_delta_module(S, T)
+    
+    def __call__(self, S: Graph, T: Graph, min_delta=2):
+        sig_s = S.signature
+        sig_t = T.signature
+        if (sig_s, sig_t, min_delta) in self.visited_paths:
+            return self.visited_paths[sig_s, sig_t, min_delta].copy()
+        else:
+            S_gmin = path_relinking(S, T)
+            self.visited_paths[sig_s, sig_t, min_delta] = S_gmin
+            return S_gmin
 
 
 def get_delta_solutions(S: Graph, T: Graph):
@@ -19,7 +44,7 @@ def get_delta_module(S: Graph, T: Graph):
     return sum(len(delta_machine) for delta_machine in delta_sol.values())
 
 
-def path_relinking(S: Graph, T: Graph, min_delta=2, max_iter=1000):
+def path_relinking(S: Graph, T: Graph, min_delta=2):
     
     # Initialize values
     c_gmin = S.C
@@ -27,6 +52,8 @@ def path_relinking(S: Graph, T: Graph, min_delta=2, max_iter=1000):
     delta_sol = get_delta_solutions(S, T)
     iter_count = 0
     total_lenght = sum(len(delta_machine) for delta_machine in delta_sol.values())
+    max_iter = total_lenght * 100
+    break_flag = False
     
     # Do path
     while total_lenght >= min_delta and iter_count <= max_iter:
@@ -35,19 +62,37 @@ def path_relinking(S: Graph, T: Graph, min_delta=2, max_iter=1000):
         c_min = float("inf")
         
         # Iterate over machines
-        for m in S.machines:
+        for m in np.random.permutation(S.machines):
+            
             # Iterate over swaps of machine
-            for (i, j) in delta_sol[m]:
+            for (i, j) in np.random.permutation(delta_sol[m]):
                 S_alt = S.copy()
                 S_alt.M[m].jobs.swap(i, j)
                 c_alt = calc_makespan(S_alt)
                 
-                # If better than previous update
-                if c_alt <= c_min:
+                # If strictly better than the current path solution
+                if c_alt < S_gmin.C:
                     c_min = c_alt
                     S_min = S_alt
                     best_swap = (i, j)
                     m_min = m
+                    
+                    # Add random component to explore all possibilities or not
+                    if np.random.rand() <= 0.75:
+                        break_flag = True
+                        break
+                
+                # If better than previous update
+                elif c_alt <= c_min:
+                    c_min = c_alt
+                    S_min = S_alt
+                    best_swap = (i, j)
+                    m_min = m
+            
+            # Break outside loop if step was taken
+            if break_flag:
+                break_flag = False
+                break
         
         # Update after move
         S = S_min
